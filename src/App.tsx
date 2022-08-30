398bbfcff67bb3c5a70f5f6ed136ecb2ec86d32d
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2022-08-30 23:50:35 +0600

    fixed SPA routing github issues

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import UpDown from './components/exercise-updown';
 function App() {
     return (
         <div className="App">
-            <Router>
+            <Router basename="/new-exercises-vite" />
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<HomePage />}></Route>
