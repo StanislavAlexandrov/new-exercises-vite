e582f91b96f485107b5927a03d19104c11c6946f
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2022-09-17 00:14:25 +0600

    typescript minor fix

--- src/components/custom-hooks/use-scroll-direction.tsx
+++ src/components/custom-hooks/use-scroll-direction.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useScrollDirection = () => {
-    const [scrollDirection, setScrollDirection] = useState(null);
+    const [scrollDirection, setScrollDirection] = useState('');
 
     useEffect(() => {
         let lastScrollY = window.pageYOffset;
